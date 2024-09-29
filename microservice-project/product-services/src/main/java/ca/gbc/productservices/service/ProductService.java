0d5b2023966de798c0b3d6d22a2b425cee8cd01d
TedroySimmons <tedroy.simmons@georgebrown.ca>
2024-09-28 20:29:28 -0400

    COMP3095 - Week 4

--- microservice-project/product-services/src/main/java/ca/gbc/productservices/service/ProductService.java
+++ microservice-project/product-services/src/main/java/ca/gbc/productservices/service/ProductService.java
@@ -9,6 +9,6 @@ public interface ProductService {
 
     ProductResponse createProduct(ProductRequest productRequest);
     List<ProductResponse> getALLProducts();
-    String updateProduct(String productId, ProductRequest productRequest);
-    void deleteProduct(String productId);
+    String updateProduct(String id, ProductRequest productRequest);
+    void deleteProduct(String id);
 }
